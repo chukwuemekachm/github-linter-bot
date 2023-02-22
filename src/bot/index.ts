70ebbb53f2384f9f1c17f88e36a8031de56d0452
chima <chima@valueblue.nl>
2023-02-22 13:30:26 +0100

    add pull_request test template

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,14 +1,28 @@
 
 
-import { Probot } from 'probot';
+import { Probot, ProbotOctokit, Context } from 'probot';
 
 function bot(app: Probot) {
-  app.on('issues.opened', async (context: any) => {
-    const issueComment = context.issue({
-      body: 'Thanks for opening this issue!',
-    });
-    await context.octokit.issues.createComment(issueComment);
+  app.on('pull_request.opened', async (context: Context) => {
+    console.log()
+//     const response = context.octokit.rest.pulls.listFiles({
+//   owner,
+//   repo,
+//   pull_number,
+// });
+//     const issueComment = context.octokit.pulls.createReview({
+//       body: 'Thanks for opening this issue!',
+//       owner: context.payload
+//     });
+//     await context.octokit.issues.createComment(issueComment);
   });
+  
+  // app.on('pull_request.reopened', async (context: Context) => {
+  //   const issueComment = context.octokit.pulls.createReview({
+  //     body: 'Thanks for opening this issue!',
+  //   });
+  //   await context.octokit.issues.createComment(issueComment);
+  // });
   // For more information on building apps:
   // https://probot.github.io/docs/
 
