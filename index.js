5b4e2c3c61c3e086f587e22b2c61e0e4d9853a0c
chima <chima@valueblue.nl>
2023-02-28 22:33:10 +0100

    install and configure ESLint

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Probot} app
- */
-module.exports = (app) => {
-  // Your code here
-  // app.log.info("Yay, the app was loaded!");
-  app.on(
-    ["pull_request.opened", "pull_request.synchronize"],
-    async (context) => {
-      const { repository, pull_request } = context.payload;
-      const files = await fetchAllPullRequestFiles(
-        context,
-        repository.owner.login,
-        repository.name,
-        pull_request.number
-      );
-    }
-  );
-};
+import { ESLint } from 'eslint';
 
 async function fetchPullRequestFiles(
   context,
@@ -25,7 +6,7 @@ async function fetchPullRequestFiles(
   repo,
   pull_number,
   page,
-  per_page
+  per_page,
 ) {
   const { data } = await context.octokit.rest.pulls.listFiles({
     owner,
@@ -52,13 +33,54 @@ async function fetchAllPullRequestFiles(context, owner, repo, pull_number) {
       repo,
       pull_number,
       page_number,
-      PER_PAGE
+      PER_PAGE,
     );
- 
+
     last_page_count = files.length;
     pullRequestFiles.push(...files);
     page_number++;
-  } while (last_page_count == PER_PAGE);
+  } while (last_page_count === PER_PAGE);
 
   return pullRequestFiles;
 }
+
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ */
+module.exports = (app) => {
+  // Your code here
+  app.log.info('Yay, the app was loaded!');
+  app.on(
+    ['pull_request.opened', 'pull_request.synchronize'],
+    async (context) => {
+      const { repository, pull_request } = context.payload;
+      const files = await fetchAllPullRequestFiles(
+        context,
+        repository.owner.login,
+        repository.name,
+        pull_request.number,
+      );
+      app.log.info('pull request files: ', files);
+      app.log.info(files);
+
+      (async function main() {
+        // 1. Create an instance.
+        const eslint = new ESLint();
+
+        // 2. Lint files.
+        const results = await eslint.lintFiles(['lib/**/*.js']);
+
+        // 3. Format the results.
+        const formatter = await eslint.loadFormatter('stylish');
+        const resultText = formatter.format(results);
+
+        // 4. Output it.
+        console.log('Linting result: ', resultText);
+      }()).catch((error) => {
+        process.exitCode = 1;
+        console.error('Linting errors: ', error);
+      });
+    },
+  );
+};
